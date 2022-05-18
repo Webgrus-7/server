74eb377f318667561e988ecb7018aebee667e48d
juya <kjy2967@naver.com>
2022-05-18 15:06:06 +0900

    Clean import

--- src/main/java/com/solveus/domain/entity/LikeList.java
+++ src/main/java/com/solveus/domain/entity/LikeList.java
@@ -4,7 +4,6 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.parsing.Problem;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
